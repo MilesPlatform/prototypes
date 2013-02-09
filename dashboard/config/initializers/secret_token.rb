83cacdce7ab6144ac1b4a8791bf06a608a356471
Brian McClain <brianmmcclain@gmail.com>
2013-02-09 12:53:37 -0500

    rails code generated for prototype dashboard

---
+++ dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = 'ee425471fa0bb7922b2d4583e28736d0d1692a3149fabe7845760606830a9de331a25138bc389ee24fa9aefff6d8b2e0c02813e0645dd940000d9fe95e2ddf6f'
